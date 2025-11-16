8be0a5304543df4c055a2c3a41e8e82fcd1a8bd3
sarahameed <shirosara19@gmail.com>
2025-11-16 23:40:39 +0300

    WIP: save changes before switching branches

--- controllers/users.js
+++ controllers/users.js
@@ -28,6 +28,66 @@ router.get('/current-user', async (req, res) => {
   }
 });
 
+// Get user's favorites with populated tour data
+router.get('/favorites', verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id)
+      .populate({
+        path: 'favourites.tour',
+        populate: {
+          path: 'company',
+          select: 'username email description'
+        }
+      });
+
+    res.json(user.favourites);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// Add tour to favorites
+router.post('/favorites/:tourId', verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    // Check if tour is already in favorites
+    const alreadyFavorited = user.favourites.some(
+      fav => fav.tour.toString() === req.params.tourId
+    );
+
+    if (alreadyFavorited) {
+      return res.status(400).json({ err: 'Tour already in favorites' });
+    }
+
+    // Add tour to favorites
+    user.favourites.push({ tour: req.params.tourId });
+    await user.save();
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+// Remove tour from favorites
+router.delete('/favorites/:tourId', verifyToken, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    // Remove tour from favorites
+    user.favourites = user.favourites.filter(
+      fav => fav.tour.toString() !== req.params.tourId
+    );
+
+    await user.save();
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
 router.get('/:id', async (req, res) => {
   try {
     // Get a list of all users, but only return their username and _id
